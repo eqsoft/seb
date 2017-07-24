8e9ab11b148eb192ee0e4c90b1a677d10bd2de0d
Daniel R. Schneider <daniel.schneider@let.ethz.ch>
2017-07-24 17:47:41 +0200

    Added new SEB settings allowVideoCapture (mapped to media.navigator.video.enabled) and allowAudioCapture (mapped to media.getusermedia.audiocapture.enabled), and set media.navigator.permission.disabled to true (prerequisite for video and audio capture to work in SEB, as the Firefox “allow” alert is not being displayed in SEB).

--- browser/apps/chrome/defaults/seb/preferences/prefs.js
+++ browser/apps/chrome/defaults/seb/preferences/prefs.js
@@ -44,3 +44,5 @@ user_pref("full-screen-api.enabled", true);
 user_pref("gfx.direct2d.disabled", false);
 user_pref("layers.acceleration.disabled", false);
 user_pref("browser.display.use_document_fonts", 1);
+user_pref("dom.serviceWorkers.enabled", true);
+user_pref("media.navigator.permission.disabled", true);
